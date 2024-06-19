909124cd18bd8e4b160fb0c6f80724d857b76118
hazhir <lhazhi@yahoo.com>
2024-06-19 15:55:39 +0330

    Update: Modified map coordinates and created snakes and ladders function

--- main.py
+++ main.py
@@ -5,65 +5,68 @@ naghshe = []
 def dice():
     return rnd.randint(1,6)
 
-"Map for Snakes and Ladders"
-for i in range(1,43):
-    if i in (22,38,40):
-        naghshe.append('[?]')
-    elif i in (24,25,39):
-        naghshe.append('[#]')
-    elif i in (11,14,26):
-        naghshe.append('[%]')
-    elif i in (2,16,28):
+for cells in range(42):
+    #snakes head
+    if cells in (16,28,38):
         naghshe.append('[$]')
-    else: 
+    #snakes tail
+    elif cells in (2,4,21):
+        naghshe.append('[?]')
+    #ladders botton
+    elif cells in (3, 13, 19):
+        naghshe.append('[#]')    
+    #ladders top
+    elif cells in (14, 26, 32):
+        naghshe.append('[%]')
+    else:
         naghshe.append('[-]')
 
-for i in range(len(naghshe)):
-    if i > 0:
-        if i % 6 == 0:
-            print('\n')
-    print(naghshe[i], end= '   ')
-print('\n')
-"'till here"
+for i in range(42):
+    if i % 6 == 0:
+        print('\n')
+    print(naghshe[i], end = "   ")
+print("\n")
 
-snakes = {
-    38:17, 
-    28:26,
-    16:12
-}
 
-ladders = {
-    3:11,
-    13:13,
-    19:13
-}
+def snakeNladders(player):
+    snakes = {
+        38:17, 
+        28:26,
+        16:12
+    }
+    if player in snakes.keys():
+        player -= snakes[player]
+        print("You got eaten by a SNAKE!")
+        print(f"Your place is {player}")
+
+    ladders = {
+        3:11,
+        13:13,
+        19:13
+    }
+    if player in ladders.keys():
+        player += ladders[player]
+        print("You climbed a ladder! YAY.")
+        print(f"Your place is {player}")
 
 player1 = 0
 player2 = 0
 
+#game logic: 
 while True:
 
-    p1roll = dice()
+    player1_roll = dice()
     input("p1, press enter: ")
-    print(f'player1 rolled a {p1roll}')
+    print(f'player1 rolled a {player1_roll}')
 
     if player1 < 42:
-        player1 += p1roll
+        player1 += player1_roll
         if player1 > 42:
             print('more than 42 :(\n')
-            player1 -= p1roll
-
-        if player1 in snakes.keys():
-            player1 -= snakes[player1]
-            print("You got eaten by a SNAKE!")
-            print(f"Your place is {player1}")
-            continue
-
-        if player1 in ladders.keys():
-            player1 += ladders[player1]
-            print("You climbed a ladder! YAY.")
-            print(f"Your place is {player1}")
-            continue
+            player1 -= player1_roll
+    
+    if snakeNladders():
+        continue
 
     print(f'player1\'s place is {player1}\n')
 
@@ -71,27 +74,18 @@ while True:
         print('player1 won')
         break
 
-    p2roll = dice()
+    player2_roll = dice()
     input("p2, press enter: ")
-    print(f'player2 rolled a {p2roll}')
+    print(f'player2 rolled a {player2_roll}')
 
     if player2 < 42:
-        player2 += p2roll
+        player2 += player2_roll
         if player2 > 42:
             print('more than 42 :(\n')
-            player2 -= p2roll
+            player2 -= player2_roll
 
-        if player2 in snakes.keys():
-            player2 -= snakes[player2]
-            print("You got eaten by a SNAKE!")
-            print(f"Your place is {player2}")
-            continue
-
-        if player2 in ladders.keys():
-            player2 += ladders[player2]
-            print("You climbed a ladder! YAY.")
-            print(f"Your place is {player2}")
-            continue
+    if snakeNladders():
+        continue
 
     print(f'player2\'s place is {player2}\n')
 
