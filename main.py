f7eb49c7e96ee845c29fb7cd6ba7c51ecf1722f6
hazhir <lhazhi@yahoo.com>
2024-07-06 13:55:59 +0330

    Modified player movement to follow traditional Snakes and Ladders rules

--- main.py
+++ main.py
@@ -9,30 +9,36 @@ def dice():
 
 def snl_map(player):
     naghshe.clear()
-    for cells in range(1,44):
-        #player's place
-        if cells == player:
-            naghshe.append('[D]')
-        #snakes head
-        elif cells in (16,28,38):
-            naghshe.append('[$]')
-        #snakes tail
-        elif cells in (2,4,21):
-            naghshe.append('[?]')
-        #ladders bottom
-        elif cells in (3, 13, 19):
-            naghshe.append('[#]')    
-        #ladders top
-        elif cells in (14, 26, 32):
-            naghshe.append('[%]')
-        else:
-            naghshe.append('[-]')
+    for row in range(6, 0, -1):
+        row_cells = []
+        for col in range(1, 8):
+            cell_num = (row - 1) * 7 + col
+            if cell_num <= 42:
+                if cell_num == player:
+                    row_cells.append('[p1]')
+                elif cell_num in (16, 28, 38):
+                    row_cells.append('[$]')
+                elif cell_num in (2, 4, 21):
+                    row_cells.append('[?]')
+                elif cell_num in (3, 13, 19):
+                    row_cells.append('[#]')
+                elif cell_num in (14, 26, 32):
+                    row_cells.append('[%]')
+                else:
+                    row_cells.append('[-]')
+            else:
+                row_cells.append('   ')
+        if row % 2 == 0:
+            row_cells.reverse()
+        naghshe.extend(row_cells)
 
-    for i in range(41,-1,-1):
+    for i in range(len(naghshe)):
         if (i + 1) % 7 == 0:
-            print('\n')
-        print(naghshe[i], end = "   ")
-    print("\n")
+            print(naghshe[i])
+            print()
+        else:
+            print(naghshe[i], end="    ")
+    print()
     print("---" * 13)
 
 
@@ -67,8 +73,8 @@ print("""
 """)
 
 input("    Press enter to start the Game:")
-print("\n")
-print("\tHere is the Board: ")
+print()
+print("\tHere is the Board:\n ")
 
 snl_map(player)
 
@@ -76,8 +82,11 @@ while True:
 
     player_roll = dice()
     input("\tPlayer, Press enter: ")
-    print("\n")
+    print()
     print(" " * 13, "Result: ")
+    if player == 42:
+        print(" " * 13,"You Won!")    
+        break
     print(f'\tPlayer rolled a {player_roll}')
 
     if player < 42:
@@ -85,10 +94,7 @@ while True:
         if player > 42:
             print('\tMore than 42.\n')
             player -= player_roll
-    else:
-        print(" " * 13,"You Won!")    
-        break
-    
+
     player = snakeNladders(player)
     print(f'\tPlayer\'s place is {player}\n\
     Here is your place on the board: \n')
