e082fa966542ad27835a3cb02e3ea702ff232ad4
hazhir <lhazhi@yahoo.com>
2024-07-06 11:51:54 +0330

    changed it to one player mode

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import random as rnd
 
 naghshe = []
 
-player1 = 1
-player2 = 1
+player = 1
 
 def dice():
     return rnd.randint(1,6)
@@ -12,10 +11,8 @@ def snl_map(player):
     naghshe.clear()
     for cells in range(1,44):
         #player's place
-        if cells == player1:
-            naghshe.append('[p1]')            
-        elif cells == player2:
-            naghshe.append('[p2]')
+        if cells == player:
+            naghshe.append('[D]')
         #snakes head
         elif cells in (16,28,38):
             naghshe.append('[$]')
@@ -73,50 +70,27 @@ input("    Press enter to start the Game:")
 print("\n")
 print("\tHere is the Board: ")
 
-snl_map(player="")
+snl_map(player)
 
 while True:
 
-    player1_roll = dice()
-    input("\tPlayer 1, Press enter: ")
+    player_roll = dice()
+    input("\tPlayer, Press enter: ")
     print("\n")
     print(" " * 13, "Result: ")
-    print(f'\tPlayer 1 rolled a {player1_roll}')
+    print(f'\tPlayer rolled a {player_roll}')
 
-    if player1 < 42:
-        player1 += player1_roll
-        if player1 > 42:
+    if player < 42:
+        player += player_roll
+        if player > 42:
             print('\tMore than 42.\n')
-            player1 -= player1_roll
+            player -= player_roll
+    else:
+        print(" " * 13,"You Won!")    
+        break
     
-    player1 = snakeNladders(player1)
-    print(f'\tPlayer 1\'s place is {player1}\n\
+    player = snakeNladders(player)
+    print(f'\tPlayer\'s place is {player}\n\
     Here is your place on the board: \n')
 
-    snl_map(player1)
-
-    if player1 == 42:
-        print(" " * 13,'Player 1 Won!')
-        break
-
-    player2_roll = dice()
-    input("\tPlayer 2, Press enter: ")
-    print("\n")
-    print(" " * 13, "Result: ")
-    print(f'\tPlayer 2 rolled a {player2_roll}')
-
-    if player2 < 42:
-        player2 += player2_roll
-        if player2 > 42:
-            print('\tMore than 42.\n')
-            player2 -= player2_roll
-
-    player2 = snakeNladders(player2)
-    print(f'\tPlayer 2\'s place is {player2}\n\
-    Here is your place on the board: \n')
-
-    snl_map(player2)
-
-    if player2 == 42:
-        print(" " * 13,'Player 2 Won!')
-        break
+    snl_map(player)
